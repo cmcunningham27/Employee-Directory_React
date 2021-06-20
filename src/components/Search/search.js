f78d6a35e45b50591451266ed7e399f2f2cf75c7
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-20 09:00:03 -0700

    brought in handleSortByName and set it as the value of onClick attribute of the Phone button

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './style.css';
 
-const Search = ({handleSortByName}) => {
+const Search = ({handleSortByName, handleSortByPhone}) => {
     return (
         <div className='App-search'>
             <button name='name' className='rounded' onClick={handleSortByName}>
                 Name
             </button>
-            <button name='phone' className='rounded button'>
+            <button name='phone' className='rounded button' onClick={handleSortByPhone}>
                 Phone
             </button>
             <input type='text' className='ms-auto' placeholder="Search Employee Name"/>
