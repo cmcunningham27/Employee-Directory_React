780380856236567b79a0eee096c5bafe63203798
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-19 21:50:34 -0700

    added a Wrapper component in App.js and imported it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from './components/Header/header';
+import Wrapper from './components/Wrapper/wrapper';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Wrapper backgroundColor='#ECF0F1'>
+      </Wrapper>
     </div>
-  );
-}
+  )
+};
 
 export default App;
