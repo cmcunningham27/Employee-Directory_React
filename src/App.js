0120fc10838e0478cdfb634c9c32f39b464ad9c8
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-19 21:56:25 -0700

    created Search component in App.js file and imported it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header/header';
 import Wrapper from './components/Wrapper/wrapper';
+import Search from './components/Search/search';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Wrapper backgroundColor='#ECF0F1'>
+        <Search />
       </Wrapper>
     </div>
   )
