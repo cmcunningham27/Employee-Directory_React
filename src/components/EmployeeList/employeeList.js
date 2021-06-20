d4d9520adc94ff779f9d9dc523673e73a2a29a61
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-19 22:11:31 -0700

    added image p tag child to employeeListTop div

--- src/components/EmployeeList/employeeList.js
+++ src/components/EmployeeList/employeeList.js
@@ -4,8 +4,11 @@ const EmployeeList = () => {
     return (
         <div className='App-employeeList'>
             <div className='employeeListTop'>
-
+                <p className='parg-1'>
+                    Image
+                </p>
             </div>
+            
         </div>
     )
 };
