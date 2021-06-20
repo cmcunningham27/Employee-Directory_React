332d1d3157c8e98af492cb02b5f922efaf8e2abd
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-19 21:48:06 -0700

    added 3 children in header.js file: img, h1, and p elements

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,7 +4,13 @@ import logo from '../../logo256.png';
 const Header = () => {
     return (
         <header className="App-header">
-            
+            <img src={logo} alt='Logo'/>
+            <h1>
+                Employee Directory
+            </h1>
+            <p>
+                Search for a specific employee or sort by Name or Phone
+            </p>
         </header>
     )
 };
