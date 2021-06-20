ed39c4e9e4d35a877face27b2a1f63a7f5a74e15
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-19 21:55:39 -0700

    imported style.css in wrapper.js file and created a main element that will wrap around all of its children elements

--- src/components/Wrapper/wrapper.js
+++ src/components/Wrapper/wrapper.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './style.css';
 
 const Wrapper = (props) => {
-
+    return (
+        <main className="App-wrapper" style={{ background: `${props.backgroundColor}`}}>
+            {props.children}
+        </main>
+    )
 };
 
 export default Wrapper;
