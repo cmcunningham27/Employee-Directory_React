f27568009e009898f877d094b93b55fbfe682e31
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-19 22:06:36 -0700

    created EmployeeList component in App.js sending employees array as prop and imported it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Header from './components/Header/header';
 import Wrapper from './components/Wrapper/wrapper';
 import Search from './components/Search/search';
+import EmployeeList from './components/EmployeeList/employeeList';
 import './App.css';
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
         <Header />
         <Wrapper backgroundColor='#ECF0F1'>
           <Search />
+          <EmployeeList employees={this.state.employees} />
         </Wrapper>
       </div>
   )
