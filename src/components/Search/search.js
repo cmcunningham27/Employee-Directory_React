98f8375ac2cd2cde73a9fb3d430ac71d42ca85f0
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-19 21:59:55 -0700

    added button for Name as first child

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Search = () => {
     return (
         <div className='App-search'>
-            
+            <button name='name' className='rounded'>
+                Name
+            </button>
         </div>
     )
 };
