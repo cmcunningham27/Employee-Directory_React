edf5176090ae23098210099efa11fd6472c0c168
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-19 21:46:43 -0700

    added a header parent element in header.js

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,7 +2,11 @@ import React from 'react';
 import logo from '../../logo256.png';
 
 const Header = () => {
-    
+    return (
+        <header className="App-header">
+            
+        </header>
+    )
 };
 
 export default Header;
