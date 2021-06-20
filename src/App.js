604c43b65e6cfaca6b8ba2e07d3868a39a199007
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-19 22:05:01 -0700

    imported Component since App will be a class component, added employees' empty array to state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import React, {Component} from 'react';
 import Header from './components/Header/header';
 import Wrapper from './components/Wrapper/wrapper';
 import Search from './components/Search/search';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Wrapper backgroundColor='#ECF0F1'>
-        <Search />
-      </Wrapper>
-    </div>
+class App extends Component {
+  state = {
+    employees: []
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+        <Wrapper backgroundColor='#ECF0F1'>
+          <Search />
+        </Wrapper>
+      </div>
   )
+  }
+  
 };
 
 export default App;
