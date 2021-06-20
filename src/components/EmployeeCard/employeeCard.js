e5a8a8126a38b7febf777391775e05c2c1bfc384
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-19 22:29:58 -0700

    created first 2 children/ image and name elements

--- src/components/EmployeeCard/employeeCard.js
+++ src/components/EmployeeCard/employeeCard.js
@@ -3,7 +3,10 @@ import React from 'react';
 const EmployeeCard = ({picture, name, phone, email, dob}) => {
     return (
         <div className='App-employeeCard'>
-            
+            <img className='employeeImage' src={picture} alt='employee'/>
+            <p className="name">
+                {name}
+            </p>
         </div>
     )
 };
