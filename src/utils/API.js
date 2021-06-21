6ee9d1702915195a62bca739bc5b5acb5db9133f
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-21 12:33:49 -0700

    attached API request to a variable and then exported it to remove the warning I was receiving in DevTools

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-//collects employees' information from Random User API
-export default {
+//collects specific employees' information from Random User API
+const API = {
     getEmployees: function() {
         return axios.get('https://randomuser.me/api/?results=50&nat=us&inc=picture,name,phone,email,dob')
     }
 };
+
+export default API;
