398136d2541d4ba0246a8a9cd3d9bd431caee1f4
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-21 11:57:53 -0700

    added comment

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//collects employees' information from Random User API
 export default {
     getEmployees: function() {
         return axios.get('https://randomuser.me/api/?results=50&nat=us&inc=picture,name,phone,email,dob')
