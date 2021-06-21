1fa6f6479c0d17dfb0b526486eac6f62b76e7efe
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-21 11:57:12 -0700

    added comments

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//brings in bootstrap to use in components
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+//sends app component to correct location in the index.html
 ReactDOM.render(
   <React.StrictMode>
     <App />
