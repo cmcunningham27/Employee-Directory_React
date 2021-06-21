fbcdb63df18ddc4d68c3a8e5e24a8c7cb4225797
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-21 12:01:12 -0700

    added comment

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
 
+//displays sort and search elements that utilize functions and state from App.js to sort employees name or phone, and display employees based off of what the user enters in the input field
 const Search = ({handleSortByName, handleSortByPhone, handleByName, searchTerm}) => {
     return (
         <div className='App-search'>
