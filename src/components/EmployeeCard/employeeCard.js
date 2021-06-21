9baf856814a0e26e41e4eaf12ac14eab93eef51a
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-21 12:04:46 -0700

    added comment

--- src/components/EmployeeCard/employeeCard.js
+++ src/components/EmployeeCard/employeeCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
 
+//displays an image, name, phone number, email, and date of birth for each employee
 const EmployeeCard = ({picture, name, phone, email, dob}) => {
     return (
         <div className='App-employeeCard'>
